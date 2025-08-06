d0f6b904c7fa974dba19c814d8b04f2f6c80b629
MHT <hassantariqirshad@gmail.com>
2025-08-06 13:02:07 +0500

    responsive nav bar setup

--- src/lib/comps/NavBar.svelte
+++ src/lib/comps/NavBar.svelte
@@ -2,20 +2,48 @@
     let routeText: string[] = ["Home", "Departments", "Other Services", "About Us", "Contact Us"]
     let routeLink: string[] = ["", "departments", "other-services", "about-us", "contact"]
 
+    function slideMenu(): void{
+        const ul:HTMLUListElement = document.getElementsByTagName("ul")[0];
+        ul.style = "right: 0px;"
+        console.log("done")
+    }
+
+    function closeMenu(): void{
+        const ul:HTMLUListElement = document.getElementsByTagName("ul")[0];
+        ul.style.position = "absolute";
+        ul.style.right = "-1000px";
+        console.log("done")
+    }
+
 </script>
 
 <nav>
     <img src="/images/logo.png" alt="logo of bmc">
 
-    <ul class="inter-500">
-        
-        {#each routeText as route, index}
-            <li>
-                <a href="/{routeLink[index]}">{route}</a>
-            </li>
-        {/each}
+    <div id="list-container">
+        <ul class="inter-500">
+            
+            <button id="close-menu" on:click={closeMenu}>
+                <span class="material-symbols-outlined">
+                    close
+                </span>
+            </button>
 
-    </ul>
+            {#each routeText as route, index}
+                <li>
+                    <a href="/{routeLink[index]}">{route}</a>
+                </li>
+            {/each}
+
+        </ul>
+    </div>
+    
+
+    <button id="open-menu" on:click={slideMenu}>
+        <span class="material-symbols-outlined">
+            menu
+        </span>
+</button>
 
 </nav>
 
@@ -23,7 +51,7 @@
 
     nav{
         width: 100%;
-        padding: 10px;
+        padding: 20px;
 
         display: flex;
         justify-content: space-between;
@@ -37,10 +65,9 @@
     }
 
     img{
-        width: 10rem;
-        height: auto;
+        width: 8rem;
 
-        position: absolute;
+        position: relative;
         left: 0;
         top: 0;
     }
@@ -53,6 +80,8 @@
         list-style-type: none;
 
         margin-left: auto;
+
+        transition: ease-in 500ms;
     }
 
     li{
@@ -69,10 +98,61 @@
         transition: all ease-in 200ms;
     }
 
+    button:hover{
+        cursor:pointer;
+    }
+
+    span{
+        font-size: 3.2rem;
+    }
+
+    #open-menu{
+        background-color: transparent;
+        display: none;
+        border: none;
+    }
+
+    #close-menu{
+        background-color: transparent;
+        border: none;
+        position: absolute;
+        top: 0;
+        right: 0;
+        display: none;
+
+        margin: 20px;
+    }
+
     a:hover{
         color: var(--accent-red);
     }
 
-    @media 
+    @media (width < 900px){
+        ul{
+
+            position: fixed;
+            width: 100%;
+            height: 100vh;
+            flex-direction: column;
+            position:absolute;
+            top: 10px;
+            right: -1000px;
+
+            padding: 50px;
+
+            background-color: rgba(255, 255, 255);
+            
+        }
+
+        a{
+            font-size: 1.6rem;
+        }
+
+        #open-menu, #close-menu{
+            display: inline;
+        }
+
+
+    }
 
 </style>
